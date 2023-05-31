73176340c8b114f725818923909bb42c1f4d257a
Emerson <emersnlucenaoficial@gmail.com>
2023-05-31 16:00:01 -0300

    🔒️ Consume sensitive data from .env file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CodeChallengeController } from './code-challenge/code-challenge.controller';
-import { CoffeeEntity } from './code-challenge/code-challenge.entity';
 import { CodeChallengeModule } from './code-challenge/code-challenge.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'mvst-coffee-tea-challenge-db',
-      entities: [CoffeeEntity],
-      synchronize: true,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: true, //turn it false on production
     }),
     CodeChallengeModule,
   ],
