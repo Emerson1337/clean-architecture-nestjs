c9d8489e2a60fded69a1c1a0c011bdb8aaced90d
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 16:32:13 -0300

    🥅 Add patterns to return error and success reponses

--- backend/src/presentation/errors/missing-param-error.ts
+++ backend/src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,9 @@
 export class MissingParamError extends Error {
-  constructor(paramName: string) {
+  status: number;
+  constructor(paramName: string, statusCode = 400) {
     super(`Missing param: ${paramName}`);
     this.name = 'MissingParamError';
+    this.message = `Missing param: ${paramName}`;
+    this.status = statusCode;
   }
 }
--- backend/src/presentation/errors/server-error.ts
+++ backend/src/presentation/errors/server-error.ts
@@ -1,7 +1,9 @@
 export class ServerError extends Error {
-  constructor(stack?: string) {
+  status: number;
+  constructor(stack?: string, statusCode = 500) {
     super('Internal server error');
     this.name = 'ServerError';
     this.stack = stack;
+    this.status = statusCode;
   }
 }
