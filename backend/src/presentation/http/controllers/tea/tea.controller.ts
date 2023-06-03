e347e9d0da5bcc9913826d4217223d05732b472e
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 16:59:52 -0300

    🍱 Preserve file extension before to save on uploads

--- backend/src/presentation/http/controllers/tea/tea.controller.ts
+++ backend/src/presentation/http/controllers/tea/tea.controller.ts
@@ -11,6 +11,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { Request, Response } from 'express';
 import { handleError, ok } from '../../../helpers/http.helper';
+import { extname } from 'path';
 
 @Controller('teas')
 export class TeaController {
@@ -22,9 +23,11 @@ export class TeaController {
       storage: diskStorage({
         destination: './uploads',
         filename: (req, file, cb) => {
+          const extension = extname(file.originalname);
           const uniqueSuffix =
             Date.now() + '-' + Math.round(Math.random() * 1e9);
-          cb(null, file.fieldname + '-' + uniqueSuffix);
+          const fileName = `${file.fieldname}-${uniqueSuffix}${extension}`;
+          cb(null, fileName);
         },
       }),
     }),
