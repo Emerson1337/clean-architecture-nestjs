664fabec311d63352712b56350a3fd3d3bafb0e8
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 15:17:23 -0300

    ✨ Add list coffees and create coffees feature

---
+++ backend/src/application/use-cases/coffee/repositories/coffee.repository.ts
@@ -0,0 +1,8 @@
+import { Coffee } from '../../../../domain/entities/coffee.entity';
+import { CreateProductDto } from '../../../dtos/create-product-dto';
+
+export abstract class CoffeeRepository {
+  abstract create(coffee: CreateProductDto): Promise<Coffee>;
+  abstract get(query?: any): Promise<Array<Coffee>>;
+  abstract find(query: any): Promise<Coffee>;
+}
