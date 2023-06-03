944d26053229a39e6c36a299029bc4abd52940d2
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 00:50:42 -0300

    ♻️ Move sensitive data to .env and change orm config json to js

---
+++ backend/ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: false,
+  logging: true,
+  entities: ['src/**/*.entity{.js,.ts}'],
+  migrations: ['src/database/typeorm/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/database/typeorm/migrations',
+  },
+};
