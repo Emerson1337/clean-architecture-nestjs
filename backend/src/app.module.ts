7ff60f71cf84e485c18915ce49c730bdcdeb9b8d
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 15:11:27 -0300

    🔧 Configure app modules to perform database and http functions

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CodeChallengeController } from './code-challenge/code-challenge.controller';
-import { CodeChallengeModule } from './code-challenge/code-challenge.module';
+import { HttpModule } from './presentation/http/http.module';
+import { DatabaseModule } from './infra/database/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.js,.ts}'],
-      synchronize: true, //turn it false on production
-    }),
-    CodeChallengeModule,
-  ],
-  controllers: [CodeChallengeController],
-  providers: [],
+  imports: [HttpModule, DatabaseModule],
 })
 export class AppModule {}
