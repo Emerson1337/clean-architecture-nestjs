3fa3fbfd838e8f5ac1b06d5a8f3c85f2ddfea44b
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 20:59:05 -0300

    ♻️ Turn repositories methods more specific for teas

--- backend/src/application/use-cases/tea/repositories/tea.repository.ts
+++ backend/src/application/use-cases/tea/repositories/tea.repository.ts
@@ -3,6 +3,6 @@ import { CreateProductDto } from '../../../dtos/create-product-dto';
 
 export abstract class TeaRepository {
   abstract create(coffee: CreateProductDto): Promise<Tea>;
-  abstract get(query?: any): Promise<Array<Tea>>;
-  abstract find(query: any): Promise<Tea>;
+  abstract getAll(): Promise<Array<Tea>>;
+  abstract findByName(name: string): Promise<Tea>;
 }
