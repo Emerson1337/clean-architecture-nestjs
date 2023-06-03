6403973f88affbb0be2cce06a37506fbb7fa360d
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 20:40:16 -0300

    ♻️ Move stub classes for factoris on tea test

---
+++ backend/src/application/use-cases/tea/mocks/factories.ts
@@ -0,0 +1,10 @@
+import { Tea } from '../../../../domain/entities/tea.entity';
+
+export const makeFakeTea = (): Tea => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  description: 'valid_description',
+  picture: 'valid_picture',
+  created_at: new Date('2023-06-03T03:43:54.555Z'),
+  updated_at: new Date('2023-06-03T03:43:54.555Z'),
+});
