01d00e5675aa1801d7ded7e56c0dceca6daf8281
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 17:07:08 -0300

    ♻️ Move multer config to an adapter class and use on controller

--- backend/src/presentation/http/controllers/coffee/coffee.controller.ts
+++ backend/src/presentation/http/controllers/coffee/coffee.controller.ts
@@ -8,30 +8,16 @@ import {
 } from '@nestjs/common';
 import { CoffeeService } from '../../../../application/use-cases/coffee/coffee.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
 import { Request, Response } from 'express';
 import { handleError, ok } from '../../../helpers/http.helper';
-import { extname } from 'path';
+import { FileAdapter } from '../../../../infra/adapters/fileAdapter/file.adapter';
 
 @Controller('coffees')
 export class CoffeeController {
   constructor(private readonly coffeeService: CoffeeService) {}
 
   @Post()
-  @UseInterceptors(
-    FileInterceptor('picture', {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, cb) => {
-          const extension = extname(file.originalname);
-          const uniqueSuffix =
-            Date.now() + '-' + Math.round(Math.random() * 1e9);
-          const fileName = `${file.fieldname}-${uniqueSuffix}${extension}`;
-          cb(null, fileName);
-        },
-      }),
-    }),
-  )
+  @UseInterceptors(FileInterceptor('picture', new FileAdapter().saveFile()))
   async create(@Req() request: Request, @Res() response: Response) {
     try {
       const coffee = Object.assign(request.body, {
