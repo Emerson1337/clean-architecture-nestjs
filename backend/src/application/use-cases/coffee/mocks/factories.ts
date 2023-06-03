d4ee73ad290a24c3606304341b3c3c0c3b2dd9b4
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 20:39:03 -0300

    ♻️ Move stub classes for factoris on coffee test

---
+++ backend/src/application/use-cases/coffee/mocks/factories.ts
@@ -0,0 +1,10 @@
+import { Coffee } from '../../../../domain/entities/coffee.entity';
+
+export const makeFakeCoffee = (): Coffee => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  description: 'valid_description',
+  picture: 'valid_picture',
+  created_at: new Date('2023-06-03T03:43:54.555Z'),
+  updated_at: new Date('2023-06-03T03:43:54.555Z'),
+});
