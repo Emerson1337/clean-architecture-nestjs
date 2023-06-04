cc5ff807eab318371eb69d63d13143304439ec7d
Emerson <emersnlucenaoficial@gmail.com>
2023-06-04 18:49:52 -0300

    📱 Adjust image and structure page responsiveness

--- frontend/src/pages/coffees.tsx
+++ frontend/src/pages/coffees.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { fetchCoffeeService } from '../app/coffees/application/fetchCoffeeService';
-import { CoffeeMapper } from '../app/coffees/infrastructure/coffee.mapper';
-import { CoffeePage } from '../app/coffees/presentation/pages/CoffeePage';
+import React from "react";
+import { fetchCoffeeService } from "../app/coffees/application/fetchCoffeeService";
+import { CoffeeMapper } from "../app/coffees/infrastructure/coffee.mapper";
+import { CoffeePage } from "../app/coffees/presentation/pages/CoffeePage";
+import { Navbar } from "../app/interface/presentation/pages/Navbar";
 
-export async function getStaticProps() {
+export async function getStaticProps(): Promise<any> {
   const data = await fetchCoffeeService();
   return {
     props: {
@@ -14,5 +15,10 @@ export async function getStaticProps() {
 
 export default function Index({ data }: any): JSX.Element {
   const domainData = data.map(CoffeeMapper.toDomain);
-  return <CoffeePage data={domainData} />;
+  return (
+    <>
+      <Navbar buttons={["Our Coffee", "Our Tea"]} />
+      <CoffeePage data={domainData} />
+    </>
+  );
 }
