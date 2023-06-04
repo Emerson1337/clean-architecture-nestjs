55e887d96fcb8baf55b358699f2b0a28c0ba4de1
Emerson <emersnlucenaoficial@gmail.com>
2023-06-04 15:12:41 -0300

    🗃️ Add different hosts for typeorm cli and nest/typeorm cli inside the container

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST_TYPEORM,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
