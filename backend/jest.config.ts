bae38a59f23632420a5a13fc1545334ff32e7d11
Emerson <emersnlucenaoficial@gmail.com>
2023-06-03 22:00:01 -0300

    🧑‍💻 Add path mapping

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -8,4 +8,11 @@ module.exports = {
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/../src/$1',
+    '^@presentation/(.*)$': '<rootDir>/../src/presentation/$1',
+    '^@application/(.*)$': '<rootDir>/../src/application/$1',
+    '^@infra/(.*)$': '<rootDir>/../src/infra/$1',
+    '^@domain/(.*)$': '<rootDir>/../src/domain/$1',
+  },
 };
