a3055d96b78397eb6a93e96a44363233257f32ef
Emerson <emersnlucenaoficial@gmail.com>
2023-06-04 18:49:52 -0300

    📱 Adjust image and structure page responsiveness

--- frontend/src/app/interface/presentation/pages/Navbar.tsx
+++ frontend/src/app/interface/presentation/pages/Navbar.tsx
@@ -9,17 +9,13 @@ type Props = {
 export const Navbar: React.FC<Props> = ({ buttons }) => {
   return (
     <div className="navbar">
-      <div className="logo default-size">
-        <Image src={logo} alt="logo picture" />
+      <div className="logo-space default-size">
+        <Image layout="fixed" src={logo} alt="logo picture" />
       </div>
       <div className="menus">
         <div className="buttons">
           {buttons.map((button) => {
-            return (
-              <span className="selected" key={button}>
-                {button}
-              </span>
-            );
+            return <span key={button}>{button}</span>;
           })}
         </div>
       </div>
