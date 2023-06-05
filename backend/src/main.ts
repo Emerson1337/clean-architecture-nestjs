06f9ae3cdc5f2415d065db8a604d380115eb2e61
Emerson <emersnlucenaoficial@gmail.com>
2023-06-04 23:37:07 -0300

    🔒️ Allow cors on API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use('/uploads', express.static('uploads'));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
