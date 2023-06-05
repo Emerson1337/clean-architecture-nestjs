5fe156f04209d399d641819d6fd3f1f79df937c4
Emerson <emersnlucenaoficial@gmail.com>
2023-06-04 23:40:22 -0300

    ✨ Add fetch functions to consume API teas

--- frontend/src/pages/teas.tsx
+++ frontend/src/pages/teas.tsx
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { fetchCoffeeService } from "../app/coffees/application/fetchCoffeeService";
-import { CoffeeMapper } from "../app/coffees/infrastructure/coffee.mapper";
 import { Navbar } from "../app/interface/presentation/pages/Navbar";
 import { TeaPage } from "../app/teas/presentation/pages/TeaPage";
 import { menuPaths } from "../shared/contants/menuPaths";
 import Spinner from "../app/interface/presentation/components/Spinner";
+import { fetchTeaService } from "../app/teas/application/fetchTeaService";
+import { TeaMapper } from "../app/teas/infrastructure/tea.mapper";
+import { motion } from "framer-motion";
 
-export async function getStaticProps(): Promise<any> {
-  const data = await fetchCoffeeService();
-  return {
-    props: {
-      data,
-    },
-  };
-}
-
-export default function Index({ data }: any): JSX.Element {
+export default function Index(): JSX.Element {
   const [isLoading, setIsLoading] = useState(true);
 
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    setTimeout(() => {
+    const fetchData = async () => {
+      setData(await fetchTeaService());
       setIsLoading(false);
-    }, 1000);
+    };
+
+    fetchData();
   }, []);
 
-  const domainData = data.map(CoffeeMapper.toDomain);
+  const domainData = data.map(TeaMapper.toDomain);
   return (
     <>
       <Navbar buttons={menuPaths} />
-
-      {isLoading ? (
-        <div className="load-screen">
-          <Spinner />
-        </div>
-      ) : (
-        <TeaPage data={domainData} />
-      )}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {isLoading ? (
+          <div className="load-screen">
+            <Spinner />
+          </div>
+        ) : (
+          <TeaPage data={domainData} />
+        )}
+      </motion.div>
     </>
   );
 }
