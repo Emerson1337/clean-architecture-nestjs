97cc53b5e4df737cd5d2805f4efc322f6ee4a41b
Emerson <emersnlucenaoficial@gmail.com>
2023-06-04 23:40:33 -0300

    👽️ Configure API service

---
+++ frontend/src/services/config/apiConfig.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { apiAddress } from "../../shared/contants/apiAddresses";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || apiAddress;
+
+export const API = axios.create({
+  baseURL: BASE_URL,
+});
