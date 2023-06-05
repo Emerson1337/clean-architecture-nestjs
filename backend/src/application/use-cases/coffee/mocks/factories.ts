63d9eea537e4ac23706cb1c2e96297b40c4becc2
Emerson <emersnlucenaoficial@gmail.com>
2023-06-04 22:23:58 -0300

    🤡 Fix factory coffee for tests and add validator

--- backend/src/application/use-cases/coffee/mocks/factories.ts
+++ backend/src/application/use-cases/coffee/mocks/factories.ts
@@ -5,6 +5,7 @@ export const makeFakeCoffee = (): Coffee => ({
   name: 'valid_name',
   description: 'valid_description',
   picture: 'valid_picture',
+  type: 'ROBUSTA',
   created_at: new Date('2023-06-03T03:43:54.555Z'),
   updated_at: new Date('2023-06-03T03:43:54.555Z'),
 });
